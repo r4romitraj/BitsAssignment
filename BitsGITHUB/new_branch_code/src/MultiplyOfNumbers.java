4fca8f7f891abfb59fee019a6e64c09e405513f0
r4romitraj <r4romitraj@gmail.com>
2023-03-02 02:02:07 +0530

    Commit for multiplication program

---
+++ BitsGITHUB/new_branch_code/src/MultiplyOfNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MultiplyOfNumbers {
+    public static void main(String[] args) {
+
+        int a = 0,b=0,Multiplication=0;
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Enter first number");
+        a=sc.nextInt();
+        System.out.println("Enter second number");
+        b=sc.nextInt();
+        Multiplication =a*b;
+        System.out.println("Multiplication of two number is :" +Multiplication);
+
+
+    }
+}
