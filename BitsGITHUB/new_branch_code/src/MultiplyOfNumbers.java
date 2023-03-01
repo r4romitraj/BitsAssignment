8577aaec1b614de381f3a683ff667955027e7f1c
r4romitraj <r4romitraj@gmail.com>
2023-03-02 02:30:50 +0530

    adding division function

--- BitsGITHUB/new_branch_code/src/MultiplyOfNumbers.java
+++ BitsGITHUB/new_branch_code/src/MultiplyOfNumbers.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MultiplyOfNumbers {
     public static void main(String[] args) {
 
-        int a = 0,b=0,Multiplication=0;
+        int a = 0,b=0,Multiplication=0,Division=0;
         Scanner sc =  new Scanner(System.in);
         System.out.println("Enter first number");
         a=sc.nextInt();
@@ -11,7 +11,8 @@ public class MultiplyOfNumbers {
         b=sc.nextInt();
         Multiplication =a*b;
         System.out.println("Multiplication of two number is :" +Multiplication);
-
+        Division=a/b;
+        System.out.println("Multiplication of two number is :" +Division);
 
     }
 }
